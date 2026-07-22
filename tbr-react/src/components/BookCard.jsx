506442f585fca4679c81d4b750e5aa366762bdd0
corper18 <219812797+corper18@users.noreply.github.com>
2026-07-22 13:57:36 +0200

    Added heart button

--- tbr-react/src/components/BookCard.jsx
+++ tbr-react/src/components/BookCard.jsx
@@ -1,9 +1,20 @@
 import "../styles/BookCard.css";
 import { Link } from "react-router-dom";
+import { useState, usteState } from "react";
 
 function BookCard({ book }) {
+
+    const [isFavorite, setIsFavorite] = useState(false);
+
     return (
         <div className="book-card">
+
+            <button className={`heart-button ${isFavorite ? "active" : ""}`}
+            onClick={() => setIsFavorite(!isFavorite)}
+                >
+                    {isFavorite ? "♥" : "♡"}
+            </button>
+
             <Link to ={`/books/${book.id}`}>
                 <img className="book-cover"
                     src={book.cover}
